45e6f25463f240035a34b58f1e531f81e17bd772
Anmol Verma <anmolverma102002@gmail.com>
2022-12-27 16:56:36 +0530

    fix: :ambulance: Updated server URL

--- client/script.js
+++ client/script.js
@@ -59,7 +59,7 @@ const handleSubmit = async (e) =>{
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://avai.onrender.com/',{
     method:'POST',
     headers:{
       'Content-type' : 'application/json'
